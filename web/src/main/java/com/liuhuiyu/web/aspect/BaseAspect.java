9317086ac171a2b048c28dd25b5762998aae1307
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-01-20 15:04:28 +0800

    web 拦截器基础类 加入  HttpServletRequest HttpServletResponse 获取

---
+++ web/src/main/java/com/liuhuiyu/web/aspect/BaseAspect.java
@@ -0,0 +1,54 @@
+package com.liuhuiyu.web.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+/**
+ * 基础拦截器
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2018-06-28 16:49
+ */
+public abstract class BaseAspect {
+    /**
+     * 是否要求 ResponseBody 结构
+     * @param pjp ProceedingJoinPoint
+     * @return 是否要求 ResponseBody 结构
+     */
+    protected Boolean isResponseBody (ProceedingJoinPoint pjp){
+        MethodSignature methodSignature = (MethodSignature) pjp.getSignature();
+        Method method = methodSignature.getMethod();
+        return method.getAnnotation(ResponseBody.class)!=null;
+    }
+
+    /**
+     * 获取 HttpServletRequest
+     * @return HttpServletRequest
+     */
+    protected HttpServletRequest getHttpServletRequest() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            throw new RuntimeException("ServletRequestAttributes 空指针异常。");
+        }
+        return attributes.getRequest();
+    }
+
+    /**
+     * 获取 HttpServletResponse
+     * @return HttpServletResponse
+     */
+    protected HttpServletResponse getHttpServletResponse() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {
+            throw new RuntimeException("ServletRequestAttributes 空指针异常。");
+        }
+        return attributes.getResponse();
+    }
+}
