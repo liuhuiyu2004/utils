d533d7b7a118e7ac5968b10d99908c9de9c0d35c
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-01-22 09:27:01 +0800

    添加jpa模块

---
+++ jpa/jpa/src/main/java/com/liuhuiyu/jpa/BaseView.java
@@ -0,0 +1,98 @@
+/*
+package com.liuhuiyu.jpa;
+
+import lombok.extern.log4j.Log4j2;
+
+import javax.management.Query;
+import javax.persistence.EntityManager;
+import java.awt.print.Pageable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+*/
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2020-07-07 10:03
+ *//*
+
+@Log4j2
+public abstract class BaseView {
+    private EntityManager getEntityManager() {
+        return DaoEntityManagerFactory.getInstance().getEntityManager();
+    }
+
+
+    protected List<?> getQueryList(String sql, Map<String, Object> parameterMap, Pageable pageable) {
+        try {
+            Query query = this.getEntityManager().createNativeQuery(sql);
+            if (parameterMap != null) {
+                for (String key : parameterMap.keySet()) {
+                    query.setParameter(key, parameterMap.get(key));
+                }
+            }
+            if (pageable != null) {
+                query.setFirstResult(pageable.getPageNumber() * pageable.getPageSize());
+                query.setMaxResults(pageable.getPageSize());
+            }
+            return query.getResultList();
+        } catch (Exception ex) {
+            throw ex;
+        }
+    }
+
+    protected Long getCount(String sql, @NotNull Map<String, Object> parameterMap) {
+        Query query = this.getEntityManager().createNativeQuery(sql);
+        for (String key : parameterMap.keySet()) {
+            query.setParameter(key, parameterMap.get(key));
+        }
+        Object obj = query.getSingleResult();
+        return Long.parseLong(obj.toString());
+    }
+
+    */
+/**
+     * 第一条信息的第一列查询
+     *
+     * @param baseSql      语句
+     * @param parameterMap 参数map
+     * @return 第一条信息的第一列
+     *//*
+
+    protected Object getSingleResult(String baseSql, Map<String, Object> parameterMap) {
+        Query query = this.getEntityManager().createNativeQuery(baseSql);
+        if (parameterMap != null) {
+            for (String key : parameterMap.keySet()) {
+                query.setParameter(key, parameterMap.get(key));
+            }
+        }
+        try {
+            return query.getSingleResult();
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    */
+/**
+     * 将查询字符串转换成like字符串
+     *
+     * @param value 原始字符串
+     * @return like字符串
+     *//*
+
+    protected String getLikeString(String value) {
+        return "%" + value + "%";
+    }
+
+    public <T> List<T> getList(DaoOperator<T> b, String sql, Map<String, Object> parameterMap, Pageable pageable) {
+        List<?> list = getQueryList(sql, parameterMap, pageable);
+        List<T> resList = new ArrayList<>();
+        for (Object obj : list) {
+            resList.add(b.objectToT(obj));
+        }
+        return resList;
+    }
+}
+*/
