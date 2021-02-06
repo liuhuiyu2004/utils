442879e657e6876a9b5b3bb2720165f29a3143bc
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-02-06 11:31:28 +0800

    新加入js工具集合，通用函数

---
+++ myJs/js/progFunction.js
@@ -0,0 +1,86 @@
+/**
+ * 程序通用函数
+ * @type {{assignObj: (function(*=, *=): *), exitFullScreen: progFunction.exitFullScreen, isFullScreen: (function(): boolean), getMaxZIndex: (function(): any), fullScreen: progFunction.fullScreen}}
+ */
+progFunction = {
+    /**
+     * 进入全屏
+     */
+    fullScreen: function () {
+        let docElm = document.documentElement;
+        //W3C
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        }
+        //FireFox
+        else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+        }
+        //Chrome等
+        else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+        }
+        //IE11
+        else if (docElm.msRequestFullscreen) {
+            document.body.msRequestFullscreen();
+        }
+    },
+    /**
+     * 退出全屏
+     */
+    exitFullScreen: function () {
+        if (document.exitFullScreen) {
+            document.exitFullScreen();
+        }
+        else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        }
+        else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+        else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    },
+    /**
+     * 当前是否全屏
+     * @returns {boolean}
+     */
+    isFullScreen: function () {
+        return !!(
+            document.fullscreen ||
+            document.mozFullScreen ||
+            document.webkitIsFullScreen ||
+            document.webkitFullScreen ||
+            document.msFullScreen
+        );
+    },
+    /**
+     * 最大的 zIndex 获取
+     * @returns {number}
+     */
+    getMaxZIndex: function () {
+        return Math.max.apply(null,
+            $.map($('body *'), function (e, n) {
+                if ($(e).css('position') != 'static')
+                    return parseInt($(e).css('z-index')) || -1;
+            }));
+    },
+    /**
+     * 对象合并
+     * @param target 原始对象
+     * @param sources 加入的合并数据
+     * @returns {*}
+     */
+    assignObj: function (target, sources) {
+        let obj = target;
+        if (typeof target != 'object' || typeof sources != 'object') {
+            return sources; // 如果其中一个不是对象 就返回sources
+        }
+        for (let key in sources) {
+            // 如果target也存在 那就再次合并
+            obj[key] = target.hasOwnProperty(key) ? progFunction.assignObj(target[key], sources[key]) : sources[key];
+        }
+        return obj;
+    }
+}
