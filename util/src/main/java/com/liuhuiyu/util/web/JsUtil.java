b095ccb6bb1707d53f9eaec5d85473d244dbca72
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-03-10 08:10:33 +0800

    WEB js atob btoa java实现

--- util/src/main/java/com/liuhuiyu/util/web/JsUtil.java
+++ util/src/main/java/com/liuhuiyu/util/web/JsUtil.java
@@ -9,7 +9,8 @@ import java.util.regex.Pattern;
  * Created DateTime 2021-03-09 11:35
  */
 public class JsUtil {
-    private static final String base64hash = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    private static final String BASE_64_HASH = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    public static final String VALUE_RANGE_REGEX = "([^\\u0000-\\u00ff])";
 
     public static boolean isMatcher(String inStr, String reg) {
         Pattern pattern = Pattern.compile(reg);
@@ -24,7 +25,7 @@ public class JsUtil {
      * @return 加密后的字符串
      */
     public static String btoa(String inStr) {
-        if (inStr == null || isMatcher(inStr, "([^\\u0000-\\u00ff])")) {
+        if (inStr == null || isMatcher(inStr, VALUE_RANGE_REGEX)) {
             return null;
         }
         StringBuilder result = new StringBuilder();
@@ -37,14 +38,14 @@ public class JsUtil {
             mod = i % 3;
             switch (mod) {
                 case 0:
-                    result.append(base64hash.charAt(ascii >> 2));
+                    result.append(BASE_64_HASH.charAt(ascii >> 2));
                     break;
                 case 1:
-                    result.append(base64hash.charAt((prev & 3) << 4 | (ascii >> 4)));
+                    result.append(BASE_64_HASH.charAt((prev & 3) << 4 | (ascii >> 4)));
                     break;
                 case 2:
-                    result.append(base64hash.charAt((prev & 0x0f) << 2 | (ascii >> 6)));
-                    result.append(base64hash.charAt(ascii & 0x3f));
+                    result.append(BASE_64_HASH.charAt((prev & 0x0f) << 2 | (ascii >> 6)));
+                    result.append(BASE_64_HASH.charAt(ascii & 0x3f));
                     break;
                 default:
                     break;
@@ -54,11 +55,11 @@ public class JsUtil {
         }
 
         if (mod == 0) {
-            result.append(base64hash.charAt((prev & 3) << 4));
+            result.append(BASE_64_HASH.charAt((prev & 3) << 4));
             result.append("==");
         }
         else if (mod == 1) {
-            result.append(base64hash.charAt((prev & 0x0f) << 2));
+            result.append(BASE_64_HASH.charAt((prev & 0x0f) << 2));
             result.append("=");
         }
         return result.toString();
@@ -83,7 +84,7 @@ public class JsUtil {
         int mod;
         int i = 0;
         while (i < inStr.length()) {
-            cur = base64hash.indexOf(inStr.charAt(i));
+            cur = BASE_64_HASH.indexOf(inStr.charAt(i));
             mod = i % 4;
             switch (mod) {
                 case 1:
