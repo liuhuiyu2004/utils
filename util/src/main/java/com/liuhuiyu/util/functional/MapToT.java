d48212e2bee0d3ab1fa0da57b22603d2a0dd00ea
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-03-23 16:52:12 +0800

    添加函数式接口 ObjectToT MapToT

---
+++ util/src/main/java/com/liuhuiyu/util/functional/MapToT.java
@@ -0,0 +1,18 @@
+package com.liuhuiyu.util.functional;
+
+import java.util.Map;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-03-23 16:49
+ */
+@FunctionalInterface
+public interface MapToT<T> {
+    /**
+     * Object 转换成 类型 T
+     * @param map Map<String,Object>
+     * @return T
+     */
+    T mapToT(Map<String,Object> map);
+}
---
+++ util/src/main/java/com/liuhuiyu/util/functional/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 函数式 接口
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-03-23 16:47
+ */
+package com.liuhuiyu.util.functional;
