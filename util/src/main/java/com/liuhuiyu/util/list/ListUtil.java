f8743b99eaa1a213369e902a4232d9a6fefdda60
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-04-14 11:03:40 +0800

    add ListUtil

---
+++ util/src/main/java/com/liuhuiyu/util/list/ListUtil.java
@@ -0,0 +1,25 @@
+package com.liuhuiyu.util.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 对象转换成List
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-04-14 10:59
+ */
+public class ListUtil {
+    public static List<Object> objectToList(Object obj) {
+        List<Object> resList;
+        if (obj instanceof List<?>) {
+            List<?> list = (List<?>) obj;
+            resList=new ArrayList<>(list.size());
+            resList.addAll(list);
+        }
+        else {
+            throw new RuntimeException("无法转换");
+        }
+        return resList;
+    }
+}
