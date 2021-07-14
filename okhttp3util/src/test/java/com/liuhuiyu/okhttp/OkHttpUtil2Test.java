fde07b8356ca612004cdbd3741a4d6f296a008a8
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-07-14 13:48:51 +0800

    重写 OkHttpUtil 封装 使其更自由的使用

---
+++ okhttp3util/src/test/java/com/liuhuiyu/okhttp/OkHttpUtil2Test.java
@@ -0,0 +1,20 @@
+package com.liuhuiyu.okhttp;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-07-06 10:44
+ */
+public class OkHttpUtil2Test {
+    @Test
+    public void t() {
+        OkHttpUtil2.MethodModel methodModel = OkHttpUtil2.MethodModel.DELETE;
+        System.out.println(methodModel.getName());
+    }
+
+}
