ea2c74fff7ad05fc4a5703fa56d9a9617bdedd08
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-07-17 10:58:02 +0800

    加入 ExecutorBuilder 类 使用 ThreadPoolTaskExecutor 进行 Executor 初始化 ThreadUtil asynchronousDataLoading 加入 线程池管理

--- util/src/main/java/com/liuhuiyu/util/thread/ThreadUtil.java
+++ util/src/main/java/com/liuhuiyu/util/thread/ThreadUtil.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
+import java.util.concurrent.Executor;
 
 /**
  * 多线程工具
@@ -22,9 +23,22 @@ public class ThreadUtil {
      * Created DateTime 2021-04-14 14:03
      */
     public static <T> void asynchronousDataLoading(List<T> list, Consumer<T> consumer) {
+        asynchronousDataLoading(list,consumer, ExecutorBuilder.create().threadName("async-load-").builder());
+    }
+
+    /**
+     * 异步List循环
+     *
+     * @param list     list
+     * @param consumer list处理
+     * @param executor 线程池
+     * @author LiuHuiYu
+     * Created DateTime 2021-04-14 14:03
+     */
+    public static <T> void asynchronousDataLoading(List<T> list, Consumer<T> consumer, Executor executor) {
         List<CompletableFuture<Void>> threadPool = new ArrayList<>();
         list.forEach((item) -> {
-            CompletableFuture<Void> d = CompletableFuture.runAsync(() -> consumer.accept(item));
+            CompletableFuture<Void> d = CompletableFuture.runAsync(() -> consumer.accept(item), executor);
             threadPool.add(d);
         });
         threadPool.forEach((future) -> {
---
+++ util/src/test/java/com/liuhuiyu/util/thread/ThreadUtilTest.java
@@ -0,0 +1,36 @@
+package com.liuhuiyu.util.thread;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-07-17 10:36
+ */
+@Log4j2
+public class ThreadUtilTest {
+    @Test
+    public void asynchronousDataLoading() {
+        List<Integer> list = new ArrayList<>(5);
+        Collections.addAll(list, 5, 1, 6, 4, 3,2,7,5,6);
+        ThreadUtil.asynchronousDataLoading(list, this::testExe,ExecutorBuilder.create().corePoolSize(8).threadName("test-").builder());
+    }
+
+    private void testExe(Integer i) {
+        try {
+            log.info("开始：{}", i);
+            Thread.sleep(i * 500);
+            log.info("完成：{}", i);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
