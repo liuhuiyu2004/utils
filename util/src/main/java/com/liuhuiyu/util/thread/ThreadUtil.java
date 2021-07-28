9279c8d4716b83654aba937c965386f1862eb77e
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-07-28 19:10:50 +0800

    更合理的使用 CompletableFuture 实现异步处理

--- util/src/main/java/com/liuhuiyu/util/thread/ThreadUtil.java
+++ util/src/main/java/com/liuhuiyu/util/thread/ThreadUtil.java
@@ -1,13 +1,14 @@
 package com.liuhuiyu.util.thread;
 
+import lombok.extern.log4j.Log4j2;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
 
 /**
  * 多线程工具
@@ -16,6 +17,7 @@ import java.util.concurrent.Executor;
  * @version v1.0.0.0
  * Created DateTime 2021-04-14 14:02
  */
+@Log4j2
 public class ThreadUtil {
     /**
      * 异步List循环(等待返回结果)
@@ -39,18 +41,21 @@ public class ThreadUtil {
      * Created DateTime 2021-04-14 14:03
      */
     public static <T> void asynchronousDataLoading(@NotNull List<T> list, Consumer<T> consumer, Executor executor) {
-        @SuppressWarnings({"unchecked", "hiding"})
-        CompletableFuture<Void>[] list2 = new CompletableFuture[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            T t=list.get(i);
-            list2[i] = CompletableFuture.runAsync(() -> consumer.accept(t), executor);
-        }
-        CompletableFuture<Void> completableFuture = CompletableFuture.allOf(list2);
-        completableFuture.thenAccept((v) -> {
-        });
-        completableFuture.exceptionally((e) -> null);
+        CompletableFuture<List<CompletableFuture<Void>>> completableFutureList = CompletableFuture.supplyAsync(() -> list.stream()
+                .map(item -> CompletableFuture.runAsync(() -> consumer.accept(item), executor))
+                .collect(Collectors.toList())
+        );
         try {
-            completableFuture.get();
+            completableFutureList.thenAccept(completableFutures -> {
+                CompletableFuture<Void> completableFuture = CompletableFuture
+                        .allOf(completableFutures.toArray(new CompletableFuture[0]));
+                try {
+                    completableFuture.get();
+                }
+                catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }).get();
         }
         catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
--- util/src/test/java/com/liuhuiyu/util/thread/ThreadUtilTest.java
+++ util/src/test/java/com/liuhuiyu/util/thread/ThreadUtilTest.java
@@ -19,22 +19,36 @@ import static org.junit.Assert.*;
 public class ThreadUtilTest {
     @Test
     public void asynchronousDataLoading() {
-        List<Integer> list = new ArrayList<>(5);
-        Collections.addAll(list, 5, 1, 6, 4, 3, 2, 7, 5, 6);
-        ThreadUtil.asynchronousDataLoading(list, this::testExe);
+        for (int i = 0; i < 100; i++) {
+            List<Integer> list = new ArrayList<>(5);
+            Collections.addAll(list, 5, 1, 6, 4, 3, 2, 7, 5, 6);
+            ThreadUtil.asynchronousDataLoading(list, this::testExe);
+        }
     }
 
     @Test
     public void asynchronousDataLoading2() {
-        List<Integer> list = new ArrayList<>(5);
-        Collections.addAll(list, 5, 1, 6, 4, 3, 2, 7, 5, 6);
-        ThreadUtil.asynchronousDataLoading(list, this::testExe, ExecutorBuilder.create().corePoolSize(list.size()).threadName("test-").builder());
+//        for (int i = 0; i < 100; i++) {
+            List<Integer> list = new ArrayList<>(5);
+            Collections.addAll(list, 5, 1, 6, 4, 3, 2, 7, 5, 6);
+            ThreadUtil.asynchronousDataLoading(list, this::testExe, ExecutorBuilder.create().corePoolSize(list.size()).threadName("test-").builder());
+//        }
+//        try {
+//            Thread.sleep(20000);
+//        }
+//        catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
     }
+
 
     private void testExe(Integer i) {
         try {
             log.info("开始：{}", i);
-            Thread.sleep(i * 500);
+            Thread.sleep(i * 1000);
+//            if (i == 3) {
+//                throw new RuntimeException("3错误。");
+//            }
             log.info("完成：{}", i);
         }
         catch (InterruptedException e) {
