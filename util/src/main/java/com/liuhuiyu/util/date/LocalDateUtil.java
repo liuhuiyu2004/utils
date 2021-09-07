c35cef394d0cad37eab5d13604ac6f9c62b291bb
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-09-07 15:04:17 +0800

    加入字符串转日期时间函数

--- util/src/main/java/com/liuhuiyu/util/date/LocalDateUtil.java
+++ util/src/main/java/com/liuhuiyu/util/date/LocalDateUtil.java
@@ -1,9 +1,12 @@
 package com.liuhuiyu.util.date;
 
+import com.liuhuiyu.util.asserts.LhyAssert;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 /**
  * 本地日期时间工具
@@ -16,10 +19,46 @@ public class LocalDateUtil {
     public static String dateToString(LocalDate date, String format) {
         return date.format(DateTimeFormatter.ofPattern(format));
     }
+
     public static String dateToString(LocalDateTime date, String format) {
         return date.format(DateTimeFormatter.ofPattern(format));
     }
+
     public static String dateToString(LocalTime date, String format) {
         return date.format(DateTimeFormatter.ofPattern(format));
+    }
+
+    public static LocalDateTime stringToDateTime(String dateTimeStr) {
+        LhyAssert.assertNotNull(dateTimeStr, new DateTimeParseException("格式错误", "yyyy-mm-dd hh:MM:ss", -1));
+        final String dateTimeSeparator = " ";
+        final String dateSeparator = "-";
+        final String timeSeparator = ":";
+        int year, month, dayOfMonth, hour = 0, minute = 0, second = 0;
+        //字段拆解
+        String[] dateTimeCut = dateTimeStr.split(dateTimeSeparator);
+        LhyAssert.assertTrue(dateTimeCut.length <= 2 && dateTimeCut.length > 0, new DateTimeParseException("格式错误", "yyyy-mm-dd hh:MM:ss", -1));
+        String[] dateStr = dateTimeCut[0].split(dateSeparator);
+        LhyAssert.assertTrue(dateStr.length == 3, new DateTimeParseException("格式错误", "yyyy-mm-dd hh:MM:ss", -1));
+        try {
+            year = Integer.parseInt(dateStr[0]);
+            month = Integer.parseInt(dateStr[1]);
+            dayOfMonth = Integer.parseInt(dateStr[2]);
+        }
+        catch (NumberFormatException e) {
+            throw new DateTimeParseException("格式错误", "yyyy-mm-dd hh:MM:ss", -1);
+        }
+        if (dateTimeCut.length == 2) {
+            String[] timeStr = dateTimeCut[1].split(timeSeparator);
+            if (timeStr.length >= 1) {
+                hour = Integer.parseInt(timeStr[0]);
+            }
+            if (timeStr.length >= 2) {
+                minute = Integer.parseInt(timeStr[1]);
+            }
+            if (timeStr.length >= 3) {
+                second = Integer.parseInt(timeStr[2]);
+            }
+        }
+        return LocalDateTime.of(year, month, dayOfMonth, hour, minute, second);
     }
 }
---
+++ util/src/test/java/com/liuhuiyu/util/date/LocalDateUtilTest.java
@@ -0,0 +1,26 @@
+package com.liuhuiyu.util.date;
+
+import junit.framework.TestCase;
+import lombok.extern.log4j.Log4j2;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-09-07 14:10
+ */
+@Log4j2
+public class LocalDateUtilTest extends TestCase {
+    public void testStringToDate(){
+        String dateStr="2021-02-11";
+        LocalDateTime time2=LocalDateUtil.stringToDateTime(dateStr);
+        log.info(time2);
+//        LocalDateTime time1 = LocalDateTime.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-d H:m:s"));
+//        log.info(time1);
+    }
+
+}
