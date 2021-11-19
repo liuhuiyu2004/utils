91f432f38fd8faf2e35a6ea1b2fbed3ee4930fd6
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2021-11-19 14:22:37 +0800

    jpa 加入备注注解。

---
+++ jpa/src/main/java/com/liuhuiyu/jpa/oracle/comment/Comment.java
@@ -0,0 +1,18 @@
+package com.liuhuiyu.jpa.oracle.comment;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 数据库备注信息
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-11-19 11:35
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD})
+public @interface Comment {
+    String value() default "";
+}
