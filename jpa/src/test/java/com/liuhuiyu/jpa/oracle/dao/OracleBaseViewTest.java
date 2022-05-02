b442d71e5efc4126c8c4e46430801400aec31865
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2022-05-02 09:21:19 +0800

    添加模拟继承方法

---
+++ jpa/src/test/java/com/liuhuiyu/jpa/oracle/dao/OracleBaseViewTest.java
@@ -0,0 +1,26 @@
+package com.liuhuiyu.jpa.oracle.dao;
+
+import com.liuhuiyu.dto.IPaging;
+import com.liuhuiyu.jpa.DaoOperator;
+import com.liuhuiyu.jpa.WhereFull;
+import junit.framework.TestCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageImpl;
+
+import javax.sql.DataSource;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2022-05-02 8:56
+ */
+public class OracleBaseViewTest extends OracleBaseView {
+
+    public OracleBaseViewTest(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    public PageImpl<String> findPage(DaoOperator<String> b, IPaging t, String sql, WhereFull<IPaging> fullWhere) {
+        return new PageImplBuilder<>(b, t, sql, fullWhere).baseWhere(" WHERE(1=1)").order("").buildPage();
+    }
+}
