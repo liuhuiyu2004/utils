fab4366350b0596a1fae87842a5cf911e0422a95
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2022-05-24 09:34:01 +0800

    加入 Execution 函数接口

---
+++ util/src/main/java/com/liuhuiyu/util/functional/Execution.java
@@ -0,0 +1,16 @@
+package com.liuhuiyu.util.functional;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2022-05-23 23:06
+ */
+@FunctionalInterface
+public interface Execution {
+    /**
+     * 执行
+     * @author LiuHuiYu
+     * Created DateTime 2022-05-23 23:15
+     */
+    void run();
+}
--- util/src/test/java/com/liuhuiyu/util/bytes/ByteUtilTest.java
+++ util/src/test/java/com/liuhuiyu/util/bytes/ByteUtilTest.java
@@ -2,6 +2,8 @@ package com.liuhuiyu.util.bytes;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.ref.SoftReference;
+
 /**
  * @author LiuHuiYu
  * @version v1.0.0.0
@@ -14,17 +16,24 @@ public class ByteUtilTest {
         String s = ByteUtil.bytesOut(bytes);
 //        log.info("s={}", s);
     }
+
     @Test
     public void binary() {
         byte[] bytes = {'a', 'b', 'c'};
         String s = ByteUtil.binary(bytes, 16);
 //        log.info("s={}", s);
     }
+
     @Test
     public void string2HexUTF8() {
         String hello = ByteUtil.string2HexUTF8("大家好");
 //        log.info(hello);
-        String hello2=ByteUtil.string2HexUnicode("大家好");
+        String hello2 = ByteUtil.string2HexUnicode("大家好");
 //        log.info(hello2);
+    }
+
+    public void soft() {
+        SoftReference<byte[]> sr = new SoftReference<>(new byte[100]);
+        SoftReference<String[]> sr2 = new SoftReference<>(new String[100]);
     }
 }
