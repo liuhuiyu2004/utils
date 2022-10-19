a52a2a6c1c9c971c7b90510f4f9a55848310e037
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2022-10-19 18:22:59 +0800

    加入Tomcat端口获取方法

--- util/src/main/java/com/liuhuiyu/util/web/TomcatUtil.java
+++ util/src/main/java/com/liuhuiyu/util/web/TomcatUtil.java
@@ -1,10 +1,8 @@
 package com.liuhuiyu.util.web;
 
-import javax.management.MBeanServer;
-import javax.management.MalformedObjectNameException;
-import javax.management.ObjectName;
-import javax.management.Query;
+import javax.management.*;
 import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Optional;
 import java.util.Set;
@@ -22,7 +20,7 @@ public class TomcatUtil {
      * @author LiuHuiYu
      * Created DateTime 2022-09-29 10:51
      */
-    public static Optional<Integer> getTomcatPort() {
+    public static Optional<Integer> getTomcatPort1() {
         MBeanServer beanServer = ManagementFactory.getPlatformMBeanServer();
         try {
             Set<ObjectName> objectNames = beanServer.queryNames(new ObjectName("*:type=Connector,*"), Query.match(Query.attr("protocol"), Query.value("HTTP/1.1")));
@@ -34,8 +32,59 @@ public class TomcatUtil {
                 return Optional.of(Integer.getInteger(objectNames.iterator().next().getKeyProperty("port")));
             }
         }
-        catch (MalformedObjectNameException e) {
+        catch (Exception e) {
             return Optional.empty();
+        }
+    }
+    /**
+     * 获取tomcat启动端口
+     *
+     * @return java.util.Optional<java.lang.Integer>
+     * @author LiuHuiYu
+     * Created DateTime 2022-09-29 10:51
+     */
+    public static Optional<Integer> getTomcatPort2() {
+        MBeanServer mBeanServer = null;
+        ArrayList<MBeanServer> mBeanServers = MBeanServerFactory.findMBeanServer(null);
+        if (mBeanServers.size() > 0) {
+            mBeanServer = mBeanServers.get(0);
+        }
+        if (mBeanServer == null) {
+            return Optional.empty();
+        }
+        Set<ObjectName> objectNames = null;
+        try {
+            objectNames = mBeanServer.queryNames(new ObjectName("Catalina:type=Connector,*"), null);
+            if (objectNames == null || objectNames.size() <= 0) {
+                throw new IllegalStateException("没有发现JVM中关联的MBeanServer : " + mBeanServer.getDefaultDomain() + " 中的对象名称.");
+            }
+            for (ObjectName objectName : objectNames) {
+                String protocol = (String) mBeanServer.getAttribute(objectName, "protocol");
+                if ("HTTP/1.1".equals(protocol)
+                        || "org.apache.coyote.http11.Http11Nio2Protocol".equals(protocol)
+                        || "org.apache.coyote.http11.Http11NioProtocol".equals(protocol)) {
+                    return Optional.of((Integer) mBeanServer.getAttribute(objectName, "port"));
+                }
+            }
+        }
+        catch (Exception e) {
+            return Optional.empty();
+        }
+        return Optional.empty();
+    }
+    /**
+     * 获取tomcat启动端口
+     *
+     * @return java.util.Optional<java.lang.Integer>
+     * @author LiuHuiYu
+     * Created DateTime 2022-09-29 10:51
+     */
+    public static Optional<Integer> getTomcatPort(){
+        final Optional<Integer> tomcatPort1 = getTomcatPort1();
+        if(tomcatPort1.isPresent()){
+            return tomcatPort1;
+        }else{
+            return getTomcatPort2();
         }
     }
 }
