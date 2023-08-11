a6f4c747d8a657534c40014a5fbc4260e9a6eaaa
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2023-08-11 10:29:05 +0800

    加入工具类

--- jpa/src/main/java/com/liuhuiyu/jpa/util/DataBaseUtil.java
+++ jpa/src/main/java/com/liuhuiyu/jpa/util/DataBaseUtil.java
@@ -1,9 +1,31 @@
 package com.liuhuiyu.jpa.util;
 
+import java.io.BufferedReader;
+import java.sql.Clob;
+import java.sql.SQLException;
+
 /**
  * @author LiuHuiYu
  * @version v1.0.0.0
  * Created DateTime 2023-08-11 9:59
  */
 public class DataBaseUtil {
+    public static String clobToString(Clob clob) throws SQLException {
+        // 得到流
+        java.io.Reader is = clob.getCharacterStream();
+        BufferedReader br = new BufferedReader(is);
+        StringBuilder sb = new StringBuilder();
+        try {
+            String s = br.readLine();
+            // 执行循环将字符串全部取出付值给StringBuffer由StringBuffer转成String
+            while (s != null) {
+                sb.append(s);
+                s = br.readLine();
+            }
+        }
+        catch (Exception e) {
+            throw new DataBaseException(e);
+        }
+        return sb.toString();
+    }
 }
