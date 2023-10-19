32839160d73d39246f9903604cb326c58b768412
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2023-10-19 20:22:58 +0800

    加入数据更新接口

---
+++ model/liuhuiyu-dto/src/main/java/com/liuhuiyu/dto/IDataChange.java
@@ -0,0 +1,11 @@
+package com.liuhuiyu.dto;
+
+/**
+ * 数据更新接口
+ *
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2022-06-20 22:03
+ */
+public interface IDataChange extends IOperating {
+}
