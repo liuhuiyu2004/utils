c34d3d8b7df569de8224c5d46cf92af33987a591
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2023-10-20 14:52:29 +0800

    jpa中加入UUID默认长度接口

---
+++ model/liuhuiyu-jpa/src/main/java/com/liuhuiyu/jpa/entity/IUniversallyUniqueIdentifier.java
@@ -0,0 +1,13 @@
+package com.liuhuiyu.jpa.entity;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2023-10-20 14:22
+ */
+public interface IUniversallyUniqueIdentifier {
+    /**
+     * UUID长度
+     */
+    int COLUMN_ID_MAX_LENGTH = 32;
+}
