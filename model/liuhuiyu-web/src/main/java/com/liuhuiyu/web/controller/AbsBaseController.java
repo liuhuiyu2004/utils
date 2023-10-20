a598491c5087bdb84c79c7a8b0debadbedbe790b
liuhuyu2004 <Liuhuiyu2004@outlook.com>
2023-10-20 14:55:41 +0800

    加入web基础controller类

---
+++ model/liuhuiyu-web/src/main/java/com/liuhuiyu/web/controller/AbsBaseController.java
@@ -0,0 +1,33 @@
+package com.liuhuiyu.web.controller;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2020-12-29 8:48
+ */
+public abstract class AbsBaseController {
+    /**
+     * 首页
+     */
+    protected static final String PATH_INDEX = "/index";
+    /**
+     * 当前目录默认地址
+     */
+    protected static final String PATH_DEFAULT = "/";
+    /**
+     * 页号属性
+     */
+    public static final String ATTRIBUTE_PAGE_INDEX = "page_index";
+    /**
+     * 页面大小
+     */
+    public static final String ATTRIBUTE_PAGE_SIZE = "page_size";
+    /**
+     * id
+     */
+    public static final String ATTRIBUTE_ID = "id";
+    /**
+     * 默认页面大小
+     */
+    protected static final int DEF_PAGE_SIZE = 20;
+}
