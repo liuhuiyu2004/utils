4d44f78145882fd886d5e013b16f02db724595ab
liuhuiyu <liuhuiyu2004@outlook.com>
2024-07-03 07:14:06 +0800

    jpa基础类完善

--- model/liuhuiyu-db/liuhuiyu-jpa/src/main/java/com/liuhuiyu/jpa/entity/IUniversallyUniqueIdentifier.java
+++ model/liuhuiyu-db/liuhuiyu-jpa/src/main/java/com/liuhuiyu/jpa/entity/IUniversallyUniqueIdentifier.java
@@ -4,7 +4,9 @@ package com.liuhuiyu.jpa.entity;
  * @author LiuHuiYu
  * @version v1.0.0.0
  * Created DateTime 2023-10-20 14:22
+ * @deprecated 使用 DbConstant 替代此功能
  */
+@Deprecated
 public interface IUniversallyUniqueIdentifier {
     /**
      * UUID长度
