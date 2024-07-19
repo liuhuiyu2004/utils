f093989690c18d6b441737e9ba4bf020746e3ebe
liuhuiyu <liuhuiyu2004@outlook.com>
2024-07-19 22:43:39 +0800

    区间合并工具类，加入测试null 与 size=0返回结果

--- model/liuhuiyu-core/src/test/java/com/liuhuiyu/core/util/interval/IntervalMergerTest.java
+++ model/liuhuiyu-core/src/test/java/com/liuhuiyu/core/util/interval/IntervalMergerTest.java
@@ -47,10 +47,31 @@ class IntervalMergerTest extends TestBase {
     }
 
     @Test
-    @DisplayName("测试空列表区间")
+    @DisplayName("测试无元素列表区间")
     public void test2() {
         List<Data> intervals = new ArrayList<>();
         LOG.info("size num:{}", intervals.size());
+        LOG.info("--------------------------------------");
+        Optional<Interregional<Data>> intervalMerger1 = IntervalMerger.getFirstMergeIntervalsDetail(intervals, Comparator.comparing(Data::getB), (v, v1) -> v.getE().compareTo(v1.getB()), Comparator.comparing(Data::getE));
+        intervalMerger1.ifPresent(v -> LOG.info("findFirst list num:{},min:{},max{}", v.getElementList().size(), v.getMinValue().getB(), v.getMaxValue().getE()));
+        Optional<Interregional<Data>> intervalMerger2 = IntervalMerger.getFirstMergeIntervals(intervals, Data::getB, Data::getE, Integer::compareTo);
+        intervalMerger2.ifPresent(v -> LOG.info("findFirst list num:{},min:{},max{}", v.getElementList().size(), v.getMinValue().getB(), v.getMaxValue().getE()));
+        LOG.info("--------------------------------------");
+        final List<Interregional<Data>> interregionals = IntervalMerger.mergeIntervalsDetail(intervals, Comparator.comparing(Data::getB), (v, v1) -> v.getE().compareTo(v1.getB()), Comparator.comparing(Data::getE));
+        interregionals.forEach(intervalMerger ->
+                LOG.info("list num:{},min:{},max{}", intervalMerger.getElementList().size(), intervalMerger.getMinValue().getB(), intervalMerger.getMaxValue().getE())
+        );
+        LOG.info("--------------------------------------");
+        final List<Interregional<Data>> interregionals2 = IntervalMerger.mergeIntervals(intervals, Data::getB, Data::getE, Integer::compareTo);
+        interregionals2.forEach(intervalMerger ->
+                LOG.info("list num:{},min:{},max{}", intervalMerger.getElementList().size(), intervalMerger.getMinValue().getB(), intervalMerger.getMaxValue().getE())
+        );
+    }
+    @Test
+    @DisplayName("测试空列表区间")
+    public void test3() {
+        List<Data> intervals =null;
+        LOG.info("size num: null");
         LOG.info("--------------------------------------");
         Optional<Interregional<Data>> intervalMerger1 = IntervalMerger.getFirstMergeIntervalsDetail(intervals, Comparator.comparing(Data::getB), (v, v1) -> v.getE().compareTo(v1.getB()), Comparator.comparing(Data::getE));
         intervalMerger1.ifPresent(v -> LOG.info("findFirst list num:{},min:{},max{}", v.getElementList().size(), v.getMinValue().getB(), v.getMaxValue().getE()));
