fd85ae2ca1dd3a19b978ad4decfaa3a8e3e87991
liuhuiyu <liuhuiyu2004@outlook.com>
2024-08-31 10:12:29 +0800

    字符串工具类加入空白判定与非字符串判定

--- model/liuhuiyu-core/src/main/java/com/liuhuiyu/core/util/StringUtil.java
+++ model/liuhuiyu-core/src/main/java/com/liuhuiyu/core/util/StringUtil.java
@@ -2,18 +2,85 @@ package com.liuhuiyu.core.util;
 
 /**
  * 字符串工具类
+ *
  * @author LiuHuiYu * @version v1.0.0.0
  * Created DateTime 2024/8/28 8:49
  */
 public class StringUtil {
-
+    /**
+     * 字符串是否包含文字<p>
+     * author LiuHuiYu<p>
+     * Created DateTime 2024/8/31 10:00
+     * <pre>
+     *   str = null -> false
+     *   str = "" -> false
+     *   str = " " -> false
+     *   str = "a" -> true
+     * </pre>
+     * 检测空白字符串请使用 {@link #isEmpty(Object)}
+     * 检测不含字符的字符串请使用 {@link #isBlank(String)}
+     *
+     * @param str 字符串
+     * @return boolean
+     */
     public static boolean hasText(String str) {
         return str != null && !str.isEmpty() && containsText(str);
     }
 
+    /**
+     * 检测空白字符串<p>
+     * author LiuHuiYu<p>
+     * Created DateTime 2024/8/31 10:08
+     * <pre>
+     *     str = null -> true
+     *     str = "" -> true
+     *     str = " " -> false
+     *     str = "a" -> false
+     * </pre>
+     * 检测字符串包含文字请使用 {@link #hasText(String)}
+     * 检测不含字符的字符串请使用 {@link #isBlank(String)}
+     *
+     * @param str 检测对象
+     * @return boolean
+     */
+    public static boolean isEmpty(Object str) {
+        return str == null || CharSequenceUtil.EMPTY.equals(str);
+    }
+
+    /**
+     * 检测不含字符的字符串<p>
+     * author LiuHuiYu<p>
+     * Created DateTime 2024/8/31 10:08
+     * <pre>
+     *     str = null -> true
+     *     str = "" -> true
+     *     str = " " -> false
+     *     str = "a" -> false
+     * </pre>
+     * 检测字符串包含文字请使用 {@link #hasText(String)}
+     * 检测空白字符串请使用 {@link #isEmpty(Object)}
+     *
+     * @param str 检测对象
+     * @return boolean
+     */
+    public static boolean isBlank(String str) {
+        int strLen;
+        if (str != null && (strLen = str.length()) != 0) {
+            for (int i = 0; i < strLen; ++i) {
+                if (!Character.isWhitespace(str.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        else {
+            return true;
+        }
+    }
+
     private static boolean containsText(CharSequence str) {
         int strLen = str.length();
-        for(int i = 0; i < strLen; ++i) {
+        for (int i = 0; i < strLen; ++i) {
             if (!Character.isWhitespace(str.charAt(i))) {
                 return true;
             }
