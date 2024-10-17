4593ae18a84072ffd94aaccb684cb8ca4f07a3b0
liuhuiyu <liuhuiyu2004@outlook.com>
2024-10-17 21:09:45 +0800

    达梦数据库中修正 AbstractSqlView 实现

--- model/liuhuiyu-db/liuhuiyu-jpa-dm/src/main/java/com/liuhuiyu/jpa/dm/AbstractSqlViewDm.java
+++ model/liuhuiyu-db/liuhuiyu-jpa-dm/src/main/java/com/liuhuiyu/jpa/dm/AbstractSqlViewDm.java
@@ -1,6 +1,7 @@
 package com.liuhuiyu.jpa.dm;
 
 import com.liuhuiyu.core.help.sql.SelectSql;
+import com.liuhuiyu.core.help.sql.SelectSqlBuilder;
 import com.liuhuiyu.core.util.Assert;
 import com.liuhuiyu.dto.IComputedValueFilling;
 import com.liuhuiyu.dto.IPaging;
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -22,7 +24,7 @@ import java.util.stream.Collectors;
  * @author LiuHuiYu * @version v1.0.0.0
  * Created DateTime 2024/9/17 9:58
  */
-public class AbstractSqlViewDm<returnT extends IComputedValueFilling, findT>  extends AbstractSqlView {
+public abstract class AbstractSqlViewDm<returnT extends IComputedValueFilling, findT> extends AbstractSqlView {
     public static final String SPACE = " ";
 
     public AbstractSqlViewDm(EntityManager entityManager) {
@@ -125,6 +127,24 @@ public class AbstractSqlViewDm<returnT extends IComputedValueFilling, findT>  ex
         return new PageImpl<>(gatekeeperCarLogDtoList, iPaging.getPaging().getPageRequest(), total);
     }
 
+    /**
+     * 数据数量查询
+     *
+     * @param t         分页查询条件
+     * @param sql       基础语句
+     * @param fullWhere void fullWhere(T find, StringBuilder sqlBuilder, Map<String, Object> parameterMap) 实现
+     * @param <R>       返回分页的数据类型
+     * @param <T>       分页查询的条件
+     * @return org.springframework.data.domain.PageImpl<R>
+     * @author LiuHuiYu
+     * Created DateTime 2022-04-09 11:41
+     */
+    protected <T, R> Long count(T t, SelectSql sql, String countSql, WhereFullByParameterList<T> fullWhere) {
+        SelectSql countSql1 = sql.deepClone();
+        countSql1.setSqlBase(countSql);
+        return this.count(t, countSql1, fullWhere);
+    }
+
     public class SelectBuilder<T, R> {
         private final Class<R> clazz;
         SelectSql selectSql;
@@ -154,6 +174,7 @@ public class AbstractSqlViewDm<returnT extends IComputedValueFilling, findT>  ex
         }
 
         public long buildCount() {
+            Assert.assertTrue(StringUtils.hasText(countSql), "countSql语句未设定");
             final SelectSql selectSql1 = selectSql.deepClone();
             selectSql1.setSqlBase(countSql);
             return count(findWhere, selectSql1, fullWhere);
@@ -162,5 +183,98 @@ public class AbstractSqlViewDm<returnT extends IComputedValueFilling, findT>  ex
         public Optional<R> buildFirstResult() {
             return getFirstResult(clazz, findWhere, selectSql, fullWhere);
         }
+    }
+
+    /**
+     * 单条记录查询
+     *
+     * @param dto 查询条件
+     * @return java.util.Optional<cc.rehome.contractor_third_party.domain.AuthorityTree.dto.out.AuthorityTreeDto>
+     * @author LiuHuiYu
+     * Created DateTime 2024-01-13 17:36
+     */
+    public Optional<returnT> findOne(findT dto) {
+        WhereFullByParameterList<findT> fullWhere = this::fullWhere;
+        Class<returnT> clazz = getReturnTType();
+        Optional<returnT> optional = new SelectBuilder<>(clazz, dto, this.getSelectSql(), fullWhere)
+                .buildFirstResult();
+        optional.ifPresent(IComputedValueFilling::computedValueFilling);
+
+        return optional;
+    }
+
+    /**
+     * 单条记录查询
+     *
+     * @param dto 查询条件
+     * @return java.util.Optional<cc.rehome.contractor_third_party.domain.AuthorityTree.dto.out.AuthorityTreeDto>
+     * @author LiuHuiYu
+     * Created DateTime 2024-01-13 17:36
+     */
+    public List<returnT> findList(findT dto) {
+        WhereFullByParameterList<findT> fullWhere = this::fullWhere;
+        Class<returnT> clazz = getReturnTType();
+        List<returnT> list = new SelectBuilder<>(clazz, dto, this.getSelectSql(), fullWhere)
+                .buildList();
+        for (returnT returnT : list) {
+            returnT.computedValueFilling();
+        }
+        return list;
+    }
+
+    /**
+     * 单条记录查询
+     *
+     * @param dto 查询条件
+     * @return java.util.Optional<cc.rehome.contractor_third_party.domain.AuthorityTree.dto.out.AuthorityTreeDto>
+     * @author LiuHuiYu
+     * Created DateTime 2024-01-13 17:36
+     */
+    public PageImpl<returnT> findPage(findT dto) {
+        WhereFullByParameterList<findT> fullWhere = this::fullWhere;
+        Class<returnT> clazz = getReturnTType();
+        PageImpl<returnT> list = new SelectBuilder<>(clazz, dto, this.getSelectSql(), fullWhere)
+                .setCountSql(getCountSql())
+                .buildPage();
+        for (returnT returnT : list) {
+            returnT.computedValueFilling();
+        }
+        return list;
+    }
+
+    /**
+     * 统计记录数量查询
+     *
+     * @param dto 查询条件
+     * @return java.util.Optional<cc.rehome.contractor_third_party.domain.AuthorityTree.dto.out.AuthorityTreeDto>
+     * @author LiuHuiYu
+     * Created DateTime 2024-01-13 17:36
+     */
+    public Long findCount(findT dto) {
+        WhereFullByParameterList<findT> fullWhere = this::fullWhere;
+        Class<returnT> clazz = getReturnTType();
+        return new SelectBuilder<>(clazz, dto, this.getSelectSql(), fullWhere)
+                .setCountSql(getCountSql())
+                .buildCount();
+    }
+
+    protected SelectSql getSelectSql() {
+        return SelectSqlBuilder.createSelectSql(this.getBaseSql())
+                .withSqlWhere("WHERE(1=1)")
+                .withOrderBy(this.getOrderSql())
+                .build();
+    }
+
+    protected abstract void fullWhere(findT whereDto, SelectSql selectSql);
+
+    protected abstract String getBaseSql();
+
+    protected abstract String getCountSql();
+
+    protected abstract String getOrderSql();
+
+    @SuppressWarnings("unchecked")
+    public Class<returnT> getReturnTType() {
+        return (Class<returnT>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 }
