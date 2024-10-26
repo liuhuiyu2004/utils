1b99f4578ef5c324e86acc076dc175b0e2c0e1a3
liuhuiyu <liuhuiyu2004@outlook.com>
2024-10-26 22:02:46 +0800

    jpa_demo_model 加入 WebAddressConstant常量

---
+++ model/liuhuiyu-db/liuhuiyu-jpa-demo/liuhuiyu-jpa-demo-model/src/main/java/com/liuhuiyu/db/demo/constant/WebAddressConstant.java
@@ -0,0 +1,18 @@
+package com.liuhuiyu.db.demo.constant;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2021-11-13 14:08
+ */
+public class WebAddressConstant {
+    public static final String APPLICATION_NAME = "demo-jpa-server";
+    public static final String SWAGGER_UI_URL = "/swagger-ui/index.html";
+    public static final String HOME_ADDRESS_ROOT = "";
+    public static final String API_ADDRESS_ROOT = "/api";
+
+    public static final String COMMON_ADDRESS_ROOT = API_ADDRESS_ROOT + "/api";
+    public static final String ACCOUNT_ADDRESS_ROOT = COMMON_ADDRESS_ROOT + "/account";
+    public static final String AUTHORITY_TREE_ADDRESS_ROOT = COMMON_ADDRESS_ROOT + "/authority_tree";
+    public static final String RFID_ANALYZE_ADDRESS_ROOT = API_ADDRESS_ROOT + "/rfid_analyze";
+}
