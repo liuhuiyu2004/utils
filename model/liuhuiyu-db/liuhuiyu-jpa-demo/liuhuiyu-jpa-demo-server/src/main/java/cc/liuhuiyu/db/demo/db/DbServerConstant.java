29bb90c47142360c0b53872d5144ff0851301f3a
liuhuiyu <liuhuiyu2004@outlook.com>
2024-11-01 15:39:33 +0800

    jpa_demo_server服务加入dbOracle常量

---
+++ model/liuhuiyu-db/liuhuiyu-jpa-demo/liuhuiyu-jpa-demo-server/src/main/java/cc/liuhuiyu/db/demo/db/DbServerConstant.java
@@ -0,0 +1,14 @@
+package cc.liuhuiyu.db.demo.db;
+
+//import com.liuhuiyu.jpa.dm.constant.DbDmConstant;
+//import com.liuhuiyu.jpa.oracle.constant.DbOracleConstant;
+
+import com.liuhuiyu.jpa.oracle.constant.DbOracleConstant;
+
+/**
+ * @author LiuHuiYu * @version v1.0.0.0
+ * Created DateTime 2024/12/16 10:01
+ */
+//public interface DbServerConstant extends DbDmConstant {
+public interface DbServerConstant extends DbOracleConstant {
+}
