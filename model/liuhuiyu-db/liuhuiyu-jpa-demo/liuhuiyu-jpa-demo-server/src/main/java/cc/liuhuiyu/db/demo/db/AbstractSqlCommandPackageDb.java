51b9e9152644ef9124a1c69cd50b353472bf72bb
liuhuiyu <liuhuiyu2004@outlook.com>
2024-11-03 18:31:58 +0800

    jpa_demo_server 加入 AbstractSqlCommandPackageDb 基础包

---
+++ model/liuhuiyu-db/liuhuiyu-jpa-demo/liuhuiyu-jpa-demo-server/src/main/java/cc/liuhuiyu/db/demo/db/AbstractSqlCommandPackageDb.java
@@ -0,0 +1,25 @@
+package cc.liuhuiyu.db.demo.db;
+
+import com.liuhuiyu.core.help.sql.SelectSql;
+import com.liuhuiyu.jpa.oracle.sql.AbstractSqlCommandPackageOracle;
+
+/**
+ * @author LiuHuiYu
+ * @version v1.0.0.0
+ * Created DateTime 2024/11/29 11:52
+ */
+public abstract class AbstractSqlCommandPackageDb<T> extends AbstractSqlCommandPackageOracle<T> {
+//public abstract class AbstractSqlCommandPackageDb<T> extends AbstractSqlCommandPackageDm<T> {
+    /**
+     * SQL原生封装 构建函数
+     *
+     * @param findDto   查询条件
+     * @param selectSql selectSql
+     * @author LiuHuiYu
+     * Created DateTime 2022-11-20 8:27
+     */
+    public AbstractSqlCommandPackageDb(T findDto, SelectSql selectSql) {
+        super(findDto, selectSql);
+    }
+}
+
