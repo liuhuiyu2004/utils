ec401226c2a121771d8eb9754311fc04e0f32c8b
liuhuiyu <liuhuiyu2004@outlook.com>
2024-11-06 16:45:15 +0800

    jpa_demo_server demo加入 repository

---
+++ model/liuhuiyu-db/liuhuiyu-jpa-demo/liuhuiyu-jpa-demo-server/src/main/java/cc/liuhuiyu/db/demo/domain/demo/repository/DemoTestRepository.java
@@ -0,0 +1,11 @@
+package cc.liuhuiyu.db.demo.domain.demo.repository;
+
+import cc.liuhuiyu.db.demo.domain.demo.entity.DemoTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author LiuHuiYu * @version v1.0.0.0
+ * Created DateTime 2024/12/16 9:26
+ */
+public interface DemoTestRepository extends JpaRepository<DemoTest, String> {
+}
