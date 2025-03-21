4a8e693bd4ea6e45d6d36a24afddbb4bcc266580
liuhuiyu <liuhuiyu2004@outlook.com>
2025-03-22 00:29:23 +0800

    v3tools加入dto 工具类加入操作功能

---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-dto/src/main/java/com/liuhuiyu/dto/IComputedValueFilling.java
@@ -0,0 +1,13 @@
+package com.liuhuiyu.dto;
+
+/**
+ * 计算值填充<p>
+ * Created on 2025/3/21 21:00
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public interface IComputedValueFilling {
+    void computedValueFilling();
+}
---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-dto/src/main/java/com/liuhuiyu/dto/IDataChange.java
@@ -0,0 +1,16 @@
+package com.liuhuiyu.dto;
+
+/**
+ * 功能<p>
+ * Created on 2025/3/21 21:01
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public interface IDataChange {
+    String NONE_MODEL = Operating.NONE_MODEL;
+    String ADD_MODEL = Operating.ADD_MODEL;
+    String UPDATE_MODEL = Operating.UPDATE_MODEL;
+    String DELETE_MODEL = Operating.DELETE_MODEL;
+}
