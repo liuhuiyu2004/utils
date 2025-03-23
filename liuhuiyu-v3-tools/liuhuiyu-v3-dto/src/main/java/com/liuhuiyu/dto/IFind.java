eca83880922d8f4f411f38e02e0f12c6bb7de30c
liuhuiyu <liuhuiyu2004@outlook.com>
2025-03-23 21:02:15 +0800

    dto加入操作接口，查询接口

---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-dto/src/main/java/com/liuhuiyu/dto/IFind.java
@@ -0,0 +1,12 @@
+package com.liuhuiyu.dto;
+
+/**
+ * 查询接口<p>
+ * Created on 2025/3/23 21:00
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public interface IFind extends IOperating, IPaging  {
+}
---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-dto/src/main/java/com/liuhuiyu/dto/IOperating.java
@@ -0,0 +1,27 @@
+package com.liuhuiyu.dto;
+
+/**
+ * 功能<p>
+ * Created on 2025/3/23 21:00
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public interface IOperating {
+    /**
+     * 获取操作信息<p>
+     * author liuhuiyu<p>
+     * Created DateTime 2025/3/23 21:01
+     *
+     * @return com.liuhuiyu.dto.Operating
+     */
+    Operating getOperating();
+    /**
+     * 设置操作信息<p>
+     * author liuhuiyu<p>
+     * Created DateTime 2025/3/23 21:01
+     * @param operating 操作信息
+     */
+    void setOperating(Operating operating);
+}
