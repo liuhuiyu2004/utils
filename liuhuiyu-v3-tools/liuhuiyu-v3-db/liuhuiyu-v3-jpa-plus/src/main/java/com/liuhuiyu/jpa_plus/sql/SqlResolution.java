b2c61c416adce55ed122a793495483549a5df6ca
liuhuiyu <liuhuiyu2004@outlook.com>
2025-04-26 20:50:11 +0800

    v3 jpa-plus 加入对象转字段值

---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-db/liuhuiyu-v3-jpa-plus/src/main/java/com/liuhuiyu/jpa_plus/sql/SqlResolution.java
@@ -0,0 +1,66 @@
+package com.liuhuiyu.jpa_plus.sql;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.statement.select.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 功能<p>
+ * Created on 2025/4/26 20:49
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public class SqlResolution {
+    private final Map<String, Integer> resolutionMap;
+
+    public SqlResolution(String sql) {
+        this.resolutionMap = getSelectField(sql);
+    }
+
+    public static final int NULL_INDEX = -1;
+
+    public Integer getFieldIndex(String fieldName) {
+        return this.resolutionMap.getOrDefault(fieldName.toLowerCase(), NULL_INDEX);
+    }
+
+    public static Map<String, Integer> getSelectField(String sql) {
+        try {
+            Select stmt = (Select) CCJSqlParserUtil.parse(sql);
+            Map<String, Integer> map = new HashMap<>();
+            final AtomicInteger index = new AtomicInteger(0);
+            final AtomicInteger funcIndex = new AtomicInteger(0);
+            for (SelectItem selectItem : ((PlainSelect) stmt.getSelectBody()).getSelectItems()) {
+                selectItem.accept(new SelectItemVisitorAdapter() {
+                    @Override
+                    public void visit(SelectExpressionItem item) {
+                        String name;
+                        if (item.getAlias() != null) {
+                            name = item.getAlias().getName();
+                        }
+                        else if (item.getExpression() instanceof Column) {
+                            name = ((Column) item.getExpression()).getColumnName();
+                        }
+                        else if (item.getExpression() instanceof net.sf.jsqlparser.expression.Function) {
+                            name = String.valueOf(funcIndex.getAndIncrement());
+                        }
+                        else {
+                            return;
+                        }
+                        map.put(name.toLowerCase(), index.getAndIncrement());
+                    }
+                });
+            }
+            return map;
+        }
+        catch (JSQLParserException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
