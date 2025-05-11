e80417a3a46b7d0e1c71a3e306234d3788778a45
liuhuiyu <liuhuiyu2004@outlook.com>
2025-05-11 20:42:18 +0800

    v3 jpa-plus-demo 加入ConditionWrapper封装

---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-db/liuhuiyu-v3-jpa-plus/src/main/java/com/liuhuiyu/jpa_plus/conditions/ConditionWrapper.java
@@ -0,0 +1,12 @@
+package com.liuhuiyu.jpa_plus.conditions;
+
+/**
+ * 条件包装<p>
+ * Created on 2025/5/11 20:40
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public class ConditionWrapper {
+}
