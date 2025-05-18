fbde6eea680686b94a415a7669527d036e56673f
liuhuiyu <liuhuiyu2004@outlook.com>
2025-05-18 21:43:15 +0800

    v3 jpa-plus-demo 加入Wrapper封装

---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-db/liuhuiyu-v3-jpa-plus/src/main/java/com/liuhuiyu/jpa_plus/conditions/SelectWrapper.java
@@ -0,0 +1,12 @@
+package com.liuhuiyu.jpa_plus.conditions;
+
+/**
+ * select封装功能<p>
+ * Created on 2025/5/11 20:43
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public class SelectWrapper {
+}
---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-db/liuhuiyu-v3-jpa-plus/src/main/java/com/liuhuiyu/jpa_plus/conditions/SqlGroupByWrapper.java
@@ -0,0 +1,12 @@
+package com.liuhuiyu.jpa_plus.conditions;
+
+/**
+ * sql分组封装<p>
+ * Created on 2025/5/11 20:44
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public class SqlGroupByWrapper {
+}
