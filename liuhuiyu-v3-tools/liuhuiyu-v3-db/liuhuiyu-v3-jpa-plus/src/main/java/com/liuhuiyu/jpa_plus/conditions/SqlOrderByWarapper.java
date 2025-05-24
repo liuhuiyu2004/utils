965703f0635c0b431f8e14a39ae61505846c2a57
liuhuiyu <liuhuiyu2004@outlook.com>
2025-05-24 19:44:12 +0800

    v3 jpa-plus-demo 加入orderBy封装

---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-db/liuhuiyu-v3-jpa-plus/src/main/java/com/liuhuiyu/jpa_plus/conditions/SqlOrderByWarapper.java
@@ -0,0 +1,12 @@
+package com.liuhuiyu.jpa_plus.conditions;
+
+/**
+ * 功能<p>
+ * Created on 2025/5/24 19:43
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public class SqlOrderByWarapper {
+}
