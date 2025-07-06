94dabe94b18ba2c56936629c59303831d89a3ec1
liuhuiyu <liuhuiyu2004@outlook.com>
2025-07-06 19:35:03 +0800

    jpa-plus-demo 通用db加入常量映射

---
+++ liuhuiyu-v3-tools/liuhuiyu-v3-db/liuhuiyu-v3-jpa-plus-demo/src/main/java/com/liuhuiyu/demo/db/DbServerConstant.java
@@ -0,0 +1,15 @@
+package com.liuhuiyu.demo.db;
+
+import com.liuhuiyu.jpa_plus.constant.DbConstant;
+
+/**
+ * 数据类型常量映射<p>
+ * Created on 2025/4/20 21:14
+ *
+ * @author liuhuiyu
+ * @version 1.0
+ * @since 21
+ */
+public interface DbServerConstant
+        extends DbConstant {
+}
